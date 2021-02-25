68bf5a916316aeaf3c9a1e1c2f42bf91687358b3
Serhii Andrukh <ydfd@pop-os.localdomain>
2021-02-25 23:21:29 +0200

    [BE]: Feature basic models. Lab1.

---
+++ src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Data
+@Entity
+public class Category {
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products;
+}
---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class Product {
+    @Id
+    private Long id;
+
+    private String name;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
